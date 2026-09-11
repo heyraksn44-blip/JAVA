84a05a8f0d88d7b0358df86cfdcfd2775ef5631f
MeAmego <heyraksn44-blip@gmail.com>
2026-09-11 22:38:34 +0530

    Fix FindMax.java code content

--- FindMax.java
+++ FindMax.java
@@ -1,18 +1,14 @@
-
-D:\JAVA>git remote add origin https://github.com/heyraksn44-blip/JAVA.git
-
-D:\JAVA>git pull origin main
-remote: Enumerating objects: 3, done.
-remote: Counting objects: 100% (3/3), done.
-remote: Compressing objects: 100% (2/2), done.
-remote: Total 3 (delta 0), reused 3 (delta 0), pack-reused 0 (from 0)
-Unpacking objects: 100% (3/3), 460 bytes | 17.00 KiB/s, done.
-From https://github.com/heyraksn44-blip/JAVA
- * branch            main       -> FETCH_HEAD
- * [new branch]      main       -> origin/main
-
-D:\JAVA>git status
-On branch main
-nothing to commit, working tree clean
-
-D:\JAVA>
+public class FindMax {
+    public static void main(String[] args) {
+        int[] nums = {3, 7, 2, 9, 5};
+        int max = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        
+        System.out.println("Maximum Element: " + max);
+    }
+}
