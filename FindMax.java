bb46ac498d67d7d4d4ccb48244f186605116e55c
MeAmego <heyraksn44-blip@gmail.com>
2026-09-11 22:38:34 +0530

    Add FindMax solution

---
+++ FindMax.java
@@ -0,0 +1,18 @@
+
+D:\JAVA>git remote add origin https://github.com/heyraksn44-blip/JAVA.git
+
+D:\JAVA>git pull origin main
+remote: Enumerating objects: 3, done.
+remote: Counting objects: 100% (3/3), done.
+remote: Compressing objects: 100% (2/2), done.
+remote: Total 3 (delta 0), reused 3 (delta 0), pack-reused 0 (from 0)
+Unpacking objects: 100% (3/3), 460 bytes | 17.00 KiB/s, done.
+From https://github.com/heyraksn44-blip/JAVA
+ * branch            main       -> FETCH_HEAD
+ * [new branch]      main       -> origin/main
+
+D:\JAVA>git status
+On branch main
+nothing to commit, working tree clean
+
+D:\JAVA>
