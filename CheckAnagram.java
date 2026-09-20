c78989829a052521ce7dda9d7219abbf3cf898c3
MeAmego <heyraksn44@gmail.com>
2026-09-20 11:40:39 +0530

    Add CheckAnagram solution

---
+++ CheckAnagram.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class CheckAnagram {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        boolean isAnagram = Arrays.equals(arr1, arr2);
+
+        if (isAnagram) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+}
